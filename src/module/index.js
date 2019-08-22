4005df4f31e4296c4b4cae48759da187e6241eed
bhavya-goel <bhavya.goel@successive.tech>
2019-08-22 11:09:45 +0530

    feat: add eslint

--- src/module/index.js
+++ src/module/index.js
@@ -3,4 +3,4 @@ import { fileLoader, mergeResolvers, mergeTypes } from 'merge-graphql-schemas';
 const resolversArray = fileLoader(path.join(__dirname, './**/query.js')) 
 const typesArray = fileLoader(path.join(__dirname, './**/types.graphql'));
 export const resolver = mergeResolvers(resolversArray);
-export const deftype =  mergeTypes(typesArray, { all: true });
+export const deftype =  mergeTypes(typesArray, { all: true })

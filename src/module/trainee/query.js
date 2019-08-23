f150914a1645aefff60b37c7ea6591e99ec05407
bhavya-goel <bhavya.goel@successive.tech>
2019-08-23 17:57:14 +0530

    fix: add login to query

--- src/module/trainee/query.js
+++ src/module/trainee/query.js
@@ -4,6 +4,24 @@ export default {
       const { dataSources } = context
       const { skip, limit } = args
       return dataSources.traineeApi.getTrainee(skip, limit)
+    },
+    login (parent, args, context) {
+      const { dataSources } = context
+      const { input: { email, password } } = args
+      return dataSources.userApi.login(email, password)
+    }
+  },
+  loginResult: {
+    __resolveType (obj, context, info) {
+      if (obj.data) {
+        return 'loginResponse'
+      }
+
+      if (obj.error) {
+        return 'errorMessage'
+      }
+
+      return null
     }
   },
   getTraineeResponse: {

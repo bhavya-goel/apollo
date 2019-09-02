2ebf440fa64d079fc6d442f37629a0371fea3edc
bhavya-goel <bhavya.goel@successive.tech>
2019-09-02 11:23:12 +0530

    fix: change variable name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { Server } from './src'
 import { configuration } from './src/config'
-const server1 = new Server(configuration)
-server1.bootloader()
+const server = new Server(configuration)
+server.bootloader()

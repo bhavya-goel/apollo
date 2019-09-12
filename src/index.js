0ce6c1482aba0d827685bacfc0b4e73b28513f17
bhavya-goel <bhavya.goel@successive.tech>
2019-09-12 15:58:04 +0530

    feat: call server run explicitly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import path from 'path'
 import { fileLoader, mergeTypes } from 'merge-graphql-schemas'
+import { makeExecutableSchema } from 'apollo-server-express';
 
-export { default as resolver } from './module'
+import resolver from './module'
 
 // merge all the typedefs from all .graphql files
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'))
-export const typeDef = mergeTypes(typesArray, { all: true })
+const typeDef = mergeTypes(typesArray, { all: true })
+const schema = makeExecutableSchema({ typeDefs: typeDef, resolvers: resolver })
+
+export { schema }
 export { default as Server } from './server'

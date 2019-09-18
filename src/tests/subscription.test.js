24479f1ebde7956804ea7b69bd2b6c3fe074763e
bhavya-goel <bhavya.goel@successive.tech>
2019-09-18 11:59:26 +0530

    feat: add new test cases

--- src/tests/subscription.test.js
+++ src/tests/subscription.test.js
@@ -1,42 +1,97 @@
 import serviceData from './mockData/serviceData'
-import token from './mockData/token'
-import subscription from '../module/trainee/subscription'
-import { stub } from 'sinon'
-import { pubSub } from '../subscription'
-
-jest.useFakeTimers()
-const { createSuccess, updateSucess, deleteSuccess } = serviceData
-
-pubSub.publish('TRAINEE_ADDED', { traineeAdded: { result: createSuccess, context: token } })
-pubSub.publish('TRAINEE_UPDATED', { traineeUpdated: { result: updateSucess, context: token } })
-pubSub.publish('TRAINEE_DELETED', { traineeDeleted: { result: deleteSuccess, context: token } })
-jest.runAllTimers()
-
-const {
-  Subscription: {
-    traineeAdded,
-    traineeUpdated,
-    traineeDeleted
-  }
-} = subscription
-stub(traineeAdded, 'subscribe').returns(serviceData.createSuccess)
-stub(traineeUpdated, 'subscribe').returns(serviceData.updateSucess)
-stub(traineeDeleted, 'subscribe').returns(serviceData.deleteSuccess)
+import { schema } from '../index'
+import { graphql } from 'graphql'
+import { addMockFunctionsToSchema } from 'apollo-server-express'
 
 describe('subscription test cases', () => {
-  it('trainee Added', (done) => {
-    const result = traineeAdded.subscribe()
-    expect(result).toEqual(serviceData.createSuccess)
+  it('trainee Added', async (done) => {
+    addMockFunctionsToSchema({
+      schema: schema,
+      mocks: {
+        traineeAddResponse: () => {
+          return {
+            result: serviceData.createSuccess
+          }
+        }
+      }
+    })
+    const query = `
+      subscription{
+        traineeAdded{
+          result{
+            message
+            data{
+              name
+            }
+          }
+        }
+      }`
+    const result = await graphql(schema, query)
+    expect(result.data.traineeAdded.result).toHaveProperty('message')
+    expect(result.data.traineeAdded.result.message).toEqual('Trainee Created Successfully')
+    expect(result.data.traineeAdded.result).toHaveProperty('data')
+    expect(result.data.traineeAdded.result.data).toHaveProperty('name')
+    expect(result.data.traineeAdded.result.data.name).toEqual('y')
     done()
   })
-  it('trainee Updated', (done) => {
-    const result = traineeUpdated.subscribe()
-    expect(result).toEqual(serviceData.updateSucess)
+  it('trainee Updated', async (done) => {
+    addMockFunctionsToSchema({
+      schema: schema,
+      mocks: {
+        traineeSubscribeResponse: () => {
+          return {
+            result: serviceData.updateSucess
+          }
+        }
+      }
+    })
+    const query = `
+      subscription{
+        traineeUpdated{
+          result{
+            message
+            data{
+              id
+            }
+          }
+        }
+      }`
+    const result = await graphql(schema, query)
+    expect(result.data.traineeUpdated.result).toHaveProperty('message')
+    expect(result.data.traineeUpdated.result.message).toEqual('Trainee Updated Successfully')
+    expect(result.data.traineeUpdated.result).toHaveProperty('data')
+    expect(result.data.traineeUpdated.result.data).toHaveProperty('id')
+    expect(result.data.traineeUpdated.result.data.id).toEqual('5d7608f2a646f0427670cfe6')
     done()
   })
-  it('trainee Deleted', (done) => {
-    const result = traineeDeleted.subscribe()
-    expect(result).toEqual(serviceData.deleteSuccess)
+  it('trainee deleted', async (done) => {
+    addMockFunctionsToSchema({
+      schema: schema,
+      mocks: {
+        traineeSubscribeResponse: () => {
+          return {
+            result: serviceData.deleteSuccess
+          }
+        }
+      }
+    })
+    const query = `
+      subscription{
+        traineeDeleted{
+          result{
+            message
+            data{
+              id
+            }
+          }
+        }
+      }`
+    const result = await graphql(schema, query)
+    expect(result.data.traineeDeleted.result).toHaveProperty('message')
+    expect(result.data.traineeDeleted.result.message).toEqual('Trainee Deleted Successfully')
+    expect(result.data.traineeDeleted.result).toHaveProperty('data')
+    expect(result.data.traineeDeleted.result.data).toHaveProperty('id')
+    expect(result.data.traineeDeleted.result.data.id).toEqual('5d7608fea646f0427670cfe8')
     done()
   })
 })
